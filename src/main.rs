178885e3ea57d773a8f37b73848ebea55c20e823
Spizzy <spizzy47spizzy@gmail.com>
2023-09-19 16:16:59 +0200

    Documentation

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use terminal::EnterAlternateScreen;
 use crossterm::event::{KeyCode, Event, KeyEventKind, KeyModifiers};
 use crossterm::{event, execute};
 
+//Deriving from PartialEq for equality support
 #[derive(PartialEq)]
 enum MenuType{
     MainMenu,
@@ -32,6 +33,7 @@ enum InputState{
     Correct,
 }
 
+//Code for finding the index and next menu for the sub-menus
 impl SubMenu{
     fn next(&mut self){
         match self{
@@ -56,7 +58,7 @@ impl SubMenu{
     }
 }
 
-
+//Enables all the terminal settings and creates a new terminal instance 
 fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>>{
     let mut stdout = io::stdout();
     enable_raw_mode()?;
@@ -64,6 +66,7 @@ fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>
     Ok(Terminal::new(CrosstermBackend::new(stdout))?)
 }
 
+//Restores terminal settings and terminates terminal (by taking ownership and dereferencing)
 fn restore_terminal(
     terminal: &mut Terminal<CrosstermBackend<Stdout>>,
 ) -> Result<(), Box<dyn Error>> {
@@ -72,7 +75,10 @@ fn restore_terminal(
     Ok(terminal.show_cursor()?)
 }
 
+//Main function for input taking and terminal rendering.
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
+
+    //Data
     let mut text_list: Vec<String> = Vec::new();
     let mut start = SystemTime::now();
     let mut day = 0;
@@ -90,6 +96,8 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
     let mut text : String;
     let mut station_indexes = vec![0,1,2];
     let station_names = vec!["shelter.90", "frequency.15", "Billssurvivalguide.24", "PAT.00", "Vanta.07"];
+
+    //Reading from the text files and sorting them into the vector
     for i in 0..station_amount{
         station_dialouge.push(Vec::new());
         let content = fs::read_to_string(&format!("station{}.txt", i)[..]).expect("error");
@@ -100,12 +108,16 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
         }
     };
 
+    //Render and input loop
     Ok(loop {
+        //Unused
         if text_list.len() >= 10{
             for _i in [0..(text_list.len())-10]{
                 text_list.remove(0);
             }
         }
+
+        //Sets the current station to the station corresponding to the input
         let mut text_render: &str = "";
         if current_menu == MenuType::MainGame{
             let current_station = &station_dialouge[station_indexes[station]][day];
@@ -117,9 +129,11 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                     tempday = day;
 
                 }
+                //Sets the start index to where you were supposed to be in the text (scaled by character amount and day length)
                 start_index = min(current_station.chars().collect::<Vec<_>>().len() as usize-1,(current_station.chars().collect::<Vec<_>>().len()) * (start.elapsed()?.as_millis() as usize)/(day_length) as usize);
             }
-    
+            
+            //Formats the string depending on special characters.
             if current_station.trim() == &String::from("&"){
                 text_render = "*Nothing but static...*";
             }
@@ -130,9 +144,11 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                 text_render = "We recommend bringing food or other beneficial item’s with you and seeking shelter immediately.";
             }
             else{
-                text = current_station.chars().collect::<Vec<_>>()[start_index..min(current_station.chars().collect::<Vec<_>>().len() as usize-1,(current_station.chars().collect::<Vec<_>>().len()) * (start.elapsed()?.as_millis() as usize)/(day_length) as usize)].iter().collect();
+                //Sets the total string to go from the start index to the current time (start index is reset upon changing station, indicated by the changing of "station" and not the changing of "temp_station")
+                text = current_station.chars().collect::<Vec<_>>()[start_index..min(current_station.chars().collect::<Vec<_>>().len() as usize,(current_station.chars().collect::<Vec<_>>().len()) * (start.elapsed()?.as_millis() as usize)/(day_length) as usize)].iter().collect();
                 text_render = text.as_ref();
             }
+            //If the elapsed time is greater than the day length + a slight buffer, go to the next day.
             if start.elapsed()?.as_millis() > day_length + day_length/10{
                 start = SystemTime::now();
                 day += 1;
@@ -144,11 +160,14 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                 }
             }
         }
+        //Follow code handles rendering, see ratatui for more information
+        //Fetches size of terminal window
         let area = terminal.size().expect("Error fetching terminal size");
         let menu_title_bar : &str;
         let menu_text : &str;
-        let temp_menu_text = format!("Proceeding to day {}...", day);
-        let temp_menu_bar = format!("Day {}", day);
+        //Since same layout is used for day transitions and the main menu, set these variables to change depending on which one it currently is
+        let temp_menu_text = format!("Proceeding to day {}...", day+1);
+        let temp_menu_bar = format!("Day {}", day+1);
         if current_menu == MenuType::MainMenu{
             menu_title_bar = "The War of the Worlds Radio BroadCast";
             menu_text = "Welcome to The War of the Worlds Radio BroadCast, please use the arrow keys to navigate through the different menus, when you are done, navigate to the main menu and press any key to start playing!"
@@ -157,16 +176,30 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
             menu_text = temp_menu_text.as_ref();
             menu_title_bar = temp_menu_bar.as_ref();
         }
+
+        //Cuts the text to be max 3 lines long (gets abit screwy with text wrapping and newlines, but generally makes the text not go beyond the terminal bounds for medium sized terminals)
         let mut render_text = Text::from(text_render);
         if render_text.lines.len() > 3{
             render_text = Text::from(render_text.lines[render_text.lines.len()-3..].to_vec());
         }
 
+        //Renders the terminal
         terminal.draw(|frame| {
             match &current_menu{
-                MenuType::MainGame => {        
+                //Main game menu
+                MenuType::MainGame => {
+
+                    //Divides the menu into two rects
+                    let chunks = Layout::default()
+                        .direction(Direction::Horizontal)
+                        .margin(0)
+                        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
+                        .split(area);
+                    //Does some type conversion of the render text to make it wrap nicely. Aswell as changing the colors of the secret channels
                     let radio_text = Paragraph::new(render_text).block(Block::default().borders(Borders::ALL).title(Title::from("Radio broadcast"))).wrap(Wrap{trim : true});
-                    let mut stations : Vec<ListItem>= station_indexes.iter().map(|f| {let mut item = ListItem::new(station_names[*f]); if *f > 2 {item = item.blue()} return item}).collect();
+                    let mut stations : Vec<ListItem>= station_indexes.iter().map(|f| {let mut item = ListItem::new(station_names[station_indexes[*f]]); if *f > 2 {item = item.blue()} return item}).collect();
+                    
+                    //Sets the color of the currently selected station to be red (or magenta if it is a secret channel)
                     if station_indexes[station] > 2{
                         stations[station] = ListItem::new(format!("{}", station_names[station_indexes[station]])).magenta();
 
@@ -175,13 +208,11 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                         stations[station] = ListItem::new(format!("{}", station_names[station_indexes[station]])).light_red();
 
                     }
-                    let chunks = Layout::default()
-                        .direction(Direction::Horizontal)
-                        .margin(0)
-                        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
-                        .split(area);
                     
+
+                    //Creates the list widget for current stations by composing the List-item vector
                     let list = List::new(stations).block(Block::default().borders(Borders::ALL).title("Current station"));
+                    //Renders the text widget and the list widget
                     frame.render_widget(list, chunks[1]);
                     frame.render_widget(radio_text, chunks[0]);
                 },
@@ -189,14 +220,17 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                     let ending = Paragraph::new("Thanks for playing, press any key to exit!");
                     frame.render_widget(ending, area);
                 }
+                //Main menu and day transitions
                 _ => {
+                    //Tab vector and composing it into a widget
                     let titles = vec!["Main Menu", "Controls", "Secret..."];
                     let tabs = Tabs::new(titles).select(sub_menu.index()).style(Style::default().fg(Color::White))
                     .highlight_style(
-                        Style::default().underline_color(Color::LightRed)
-                            .add_modifier(Modifier::BOLD)
+                        Style::default()
+                            .add_modifier(Modifier::BOLD).red()
                     );
                     
+                    //renders different  menus depending on the current selected tab
                     match sub_menu {
                         SubMenu::Main => {
                             let chunks = Layout::default().constraints([Constraint::Length(1), Constraint::Percentage(95)]).margin(1).direction(Direction::Vertical).split(area);
@@ -240,8 +274,10 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
 
 
         })?;
+        //Input system
         if event::poll(Duration::from_millis(10))? {
             if let Event::Key(key) = event::read()? {
+                //Quits if you press control c or q (except when you are inputting aka in the secret menu)
                 if KeyCode::Char('q') == key.code && sub_menu != SubMenu::Secret || KeyCode::Char('c') == key.code && KeyModifiers::CONTROL == key.modifiers{
                     break;
                 }
@@ -249,6 +285,7 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                     match current_menu{
                         MenuType::MainGame => {
                             match key.code{
+                                //Sets the stations on input
                                 KeyCode::Char('1') => {station = 0},
                                 KeyCode::Char('2') => {station = 1},
                                 KeyCode::Char('3') => {station = 2},
@@ -257,24 +294,33 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                                 _ => (),
                             }
                         },
+                        //Any input during the ending quits the game
                         MenuType::Ending => {
                             break;
                         },
+                        //Day transitions and main menu
                         _ => {
+                            //Goes to the next tab or previous tab
                             match key.code{
                                 KeyCode::Right => sub_menu.next(),
                                 KeyCode::Left => sub_menu.previous(),
                                 _ => (),
                             }
+                            //Input mode for the secret menu.
                             if sub_menu == SubMenu::Secret{
                                 match key.code{
+
+                                    //Pushes input to string
                                     KeyCode::Char(c) => {
                                         input_state = InputState::Inputting;
                                         message.push(c);
                                     }
+                                    //Pops front of string when pressing backspace
                                     KeyCode::Backspace => {
                                         message.pop();
                                     }
+
+                                    //Checks if you unlocked new things or input something wrong
                                     KeyCode::Enter => {
                                         if message == String::from("Omega") || message == String::from("Vanta"){
                                             input_state = InputState::Correct;
@@ -295,6 +341,7 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                                     _ => (),
                                 }
                             }
+                            //If you are in the main menu and input anything except the tab inputs, go to to the main game.
                             else if sub_menu == SubMenu::Main && key.code != KeyCode::Right && key.code != KeyCode::Left{
                                 current_menu = MenuType::MainGame;
                                 start = SystemTime::now();
@@ -311,7 +358,7 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
     })
 }
 
-
+//Sets up, renders and restores the terminal.
 fn main(){
 
     let mut terminal = setup_terminal().expect("Error creating terminal");
